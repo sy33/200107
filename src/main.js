c5d9779868fc43cf8ab7db4e3618943ea309ab81
sooyeon <greatesj@daum.net>
2020-01-08 06:00:33 +0900

    ok

---
+++ src/main.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './main.css';
+
+
+const listImg = ["././img/bg_1.png","././img/concert.jpg" ,"././img/img4.jpg","././img/pakjinyeong.jpg"]
+const listTitle = ["채널 Num.1", "채널 Num.2", "채널 Num.3", "채널 Num.4"]
+const listTime = ["방송시간: 17:00 ~ 18:30", "방송시간: 07:00 ~ 08:00", "방송시간: 12:00 ~ 13:30", "방송시간: 00:00 ~ 01:10"]
+
+
+class Main extends Component {
+
+    render(){
+        return(
+            <div className="wrap">
+                <section className="list">
+                    
+                    <img src="././img/bg_1.png" />
+                    <h3>채널 Num.1</h3>
+                    <p>방송시간: 17:00 ~ 18:30</p>
+                </section>
+                <section className="list">
+                    <img src="././img/concert.jpg" />
+                    <h3>채널 Num.1</h3>
+                    <p>방송시간: 17:00 ~ 18:30</p>
+                </section>
+                <section className="list">
+                    <img src="././img/img4.jpg" />
+                    <h3>채널 Num.1</h3>
+                    <p>방송시간: 17:00 ~ 18:30</p>
+                </section>
+                <section className="list">
+                    <img src="././img/pakjinyeong.jpg" />
+                    <h3>채널 Num.1</h3>
+                    <p>방송시간: 17:00 ~ 18:30</p>
+                </section>
+            </div>
+        )
+    }
+}
+
+export default Main
